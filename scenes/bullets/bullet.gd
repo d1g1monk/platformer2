e4e02d0352a82ebf071da2544d690e3ac3443121
d1g1monk <c0kk0000000@gmail.com>
2025-12-17 20:14:20 +0300

    a

--- scenes/bullets/bullet.gd
+++ scenes/bullets/bullet.gd
@@ -2,9 +2,14 @@ extends Area2D
 
 var direction: Vector2
 
+func _ready() -> void:
+    var tween = get_tree().create_tween()
+    tween.tween_property($Sprite2D, "scale", Vector2(2, 2), 0.4).from(Vector2.ZERO)
+    
 func setup(pos: Vector2, dir: Vector2):
     position = pos + dir * 12
     direction = dir
     
 func _physics_process(delta: float) -> void:
-    position += direction * 30 * delta
+    position += direction * 230 * delta
+    
--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
-var speed: int = 400
+var speed: int = 200
 var direction: Vector2
 @export var jump_strength: float = 400
 @export var gravity: float = 1220
 signal shoot(pos: Vector2, dir: Vector2)
+const gun_directions: Dictionary = {
+    Vector2i(1,0)   : 0,
+    Vector2i(1,1)   : 1,
+    Vector2i(0,1)   : 2,
+    Vector2i(-1,1)  : 3,
+    Vector2i(-1,0)  : 4,
+    Vector2i(-1,-1) : 5,
+    Vector2i(0,-1)  : 6,
+    Vector2i(1,-1)  : 7,
+}
 
 func _physics_process(delta: float) -> void:
     #direction = Input.get_vector("left", "right", "jump", "shoot")
@@ -14,6 +24,7 @@ func _physics_process(delta: float) -> void:
     apply_gravity(delta)
     move_and_slide()
     animation()
+    update_marker()
     
 func apply_gravity(delta):
     velocity.y += gravity * delta
@@ -30,13 +41,21 @@ func get_input():
         shoot.emit(position, get_local_mouse_position().normalized())
         $ShootTimer.one_shot = true
         $ShootTimer.start(0.5)
-        
-
+        # Tween is short for inbetween. We will use this instead of animationplayer as this is simpler.
+        var tween = get_tree().create_tween()
+        tween.tween_property($Marker, "scale", Vector2(0.1, 0.1), 0.2)
+        tween.tween_property($Marker, "scale", Vector2(0.5, 0.5), 0.2)
+         
 func animation():
     if is_on_floor():
         $AnimationPlayer.current_animation = "run" if direction.x else "idle"
         $Legs.flip_h = direction.x < 0
     else:
         $AnimationPlayer.current_animation = "jump"
-        
-         
+    var dir = get_local_mouse_position().normalized()
+    var round_dir = Vector2i(round(dir.x), round(dir.y))
+    $Torso.frame = gun_directions[round_dir]
+    
+
+func update_marker():
+    $Marker.position = get_local_mouse_position().normalized() * 70
